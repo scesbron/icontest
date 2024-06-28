bce2e56eab56b9ba4dbe58e5d484e645fd7ee4ae
Sébastien Cesbron <sebastien@wecasa.fr>
2024-06-28 09:04:45 +0200

    add a unused icon and change the properties of an icon

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,5 +1,5 @@
 import {Gift} from "./icons.tsx";
 
 export default function Banner() {
-  return <div className="banner">Banner <Gift /></div>;
+  return <div className="banner">Banner <Gift width={32} height={32} fill={"green"} /></div>;
 }
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,5 +1,6 @@
 import { ReactComponent as Google } from './icons/google.svg'
 import { ReactComponent as Gift } from './icons/gift.svg'
 import { ReactComponent as Calendar } from './icons/calendar.svg'
+import { ReactComponent as Heart } from './icons/heart.svg'
 
-export { Google, Gift, Calendar };
+export { Google, Gift, Calendar, Heart };
