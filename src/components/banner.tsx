21e55e795d994aa3557ee333cc9b3b4d8093945d
Sébastien Cesbron <sebastien@wecasa.fr>
2024-06-28 09:08:19 +0200

    add a unused icon and change the properties of an icon

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,5 +1,5 @@
 import Icon from "./icon.tsx";
 
 export default function Banner() {
-  return <div className="banner">Banner <Icon name={'gift'}/></div>;
+  return <div className="banner">Banner <Icon name={'gift'} width={32} height={32} fill={"green"}/></div>;
 }
