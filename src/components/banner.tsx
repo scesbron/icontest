180c83f2c834108d29cfe277e61563be09e7f03e
Sébastien Cesbron <sebastien@wecasa.fr>
2024-06-28 09:11:47 +0200

    add unused icon and style a svg

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,5 +1,5 @@
 import SpriteIcon from "./sprite-icon.tsx";
 
 export default function Banner() {
-  return <div className="banner">Banner <SpriteIcon name={'gift'}/></div>;
+  return <div className="banner">Banner <SpriteIcon name={'gift'} color={'green'} size={'32'}/></div>;
 }
